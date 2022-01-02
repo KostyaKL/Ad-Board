1a3806f364b61a3b13e0d3ba999c388948c69de3
Kostya <klokshin@gmail.com>
2022-01-02 19:05:01 +0200

    init

---
+++ AdBoard/app/src/main/java/com/tkdroid/ad_board/MainActivity.java
@@ -0,0 +1,20 @@
+package com.tkdroid.ad_board;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, initializationStatus -> {
+        });
+
+    }
+}
